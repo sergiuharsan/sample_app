177810d8bfab64b60c786c1d97dcb945e5bee12e
Sergiu Harsan <122592920+sergiuharsan@users.noreply.github.com>
2024-12-10 22:55:12 +0200

    Finished static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "Hello world!"
   end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
