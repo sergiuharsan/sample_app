f1cf73f879b5ccf7c9f92b4d940324d750fa220e
Sergiu Harsan <122592920+sergiuharsan@users.noreply.github.com>
2025-02-06 22:36:18 +0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -50,6 +50,20 @@ class UsersController < ApplicationController
     redirect_to users_url, status: :see_other
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow', status: :unprocessable_entity
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow', status: :unprocessable_entity
+  end
+
 private
 
   def user_params
@@ -69,5 +83,7 @@ private
     def admin_user
       redirect_to(root_url, status: :see_other) unless current_user.admin?
     end
+
+
 
 end
